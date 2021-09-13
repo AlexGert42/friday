f48319391f5c722f3f3bb6cfd4523f879bccc5c9
MrAlexGert <alex.gert1812@.gmail.com>
2021-09-13 15:03:02 +0300

    first commit

--- src/components/content/profile/Profile.tsx
+++ src/components/content/profile/Profile.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import {Button} from "antd";
+
 
 export const Profile: React.FC = () => {
 
     return (
         <div>
-
-            <Button type="primary">ok</Button>
-
+            Profile
         </div>
     )
 }
